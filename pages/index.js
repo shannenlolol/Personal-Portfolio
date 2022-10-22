15c9123bcd2119f572ccdb8c5382081d06adfffb
Shannen Lee <shannenlee@Shannens-MacBook-Pro.local>
2022-10-22 23:47:49 +0800

    using bootstrap for nav bar

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,47 @@ export default function Home() {
   return (
     <div>
     <Head>
-      <link rel='shortcut icon' href="/icons/favicon.ico"/>
+      <link rel='shortcut icon' href="/icons/favicon.ico"></link>
+      <meta charset="utf-8"></meta>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"></link>
+      <link rel="stylesheet" href="css/styles.css"></link>
+      <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"></link>
     </Head>
-    <div>
+  <div>
+    
     <body>
-      <div>
+    <nav class="navbar bg-dark navbar-expand-lg navbar-dark">
+      <a href=""><img className={styles.signature} src= "/icons/Shannen signature.png" alt=""></img></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                          <a class="nav-link text-muted" href="" className={styles.navitem}>About Me <i class="bi bi-stars"></i> </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link text-muted" href="" className={styles.navitem}> Projects <i class="bi bi-card-text"></i> </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link text-muted" href="" className={styles.navitem}> Contact <i class="bi bi-person-lines-fill"></i></a>
+                      </li>
+                  </ul>
+                </div>
+              </nav>
+
+
+        
         <div className={styles.header}>
             <div id="stars"></div>
             <div id="stars2"></div>
             <div id="stars3"></div><br></br>
+               
+            
             <div className={styles.title}> <span> Hello! </span> <br /> <span> I'm Shannen</span></div>
         
         <div className={styles.photo_container} >
@@ -25,7 +57,7 @@ export default function Home() {
             <a className={styles.contact} href="https://t.me/shannenlolol/</div>">Let's Talk!</a>
         </div>
         <br></br>
-      </div>
+      
       <p className={styles.about_me}>Send me an email!</p>
       <br></br>
       <div>
